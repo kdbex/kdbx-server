a900b5be4bcded874b4e4b245a20e4890d616930
cuber <t.vinet@yahoo.fr>
2023-11-01 23:13:04 +0100

    util file separation

--- src/util/crypt.ts
+++ src/util/crypt.ts
@@ -1,6 +1,7 @@
+/**
+ * Crypting file to encrypt and decrypt files
+ */
 import { AES, enc } from "crypto-js";
-import fs from "fs";
-import path from "path";
 
 export function encrypt(message: string, key: string): string {
 	const encrypt = AES.encrypt(message, key);
@@ -11,16 +12,3 @@ export function decrypt(hash: string, key: string): string {
 	let bytes = AES.decrypt(hash, key);
 	return bytes.toString(enc.Utf8);
 }
-
-export interface Config {
-    filePath: string;
-    port: number;
-    token: string;
-}
-
-var config: Config = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), "config.json"), "utf-8"));
-
-export function getConfig() {
-    return config;
-}
-
--- src/util/file.ts
+++ src/util/file.ts
@@ -1,13 +1,12 @@
-import fs from "fs";
-
 /**
  * Used to export functions to manage file storages
  * All the files will be stored in the current repo when working in dev mode, and in the appdata folder when in prod mode
  */
+import fs from "fs";
+
 var dev = process.argv.includes("--dev");
 var folder = dev ? "./" : (process.env.APPDATA + "/KdbxChrome/");
 
-//fs.readFileSync(path.resolve(process.cwd(), "config.json"), "utf-8")
 //Checks if the global folder is created, if not creates it
 export function checkFolder () {
     if(!fs.existsSync(folder))
@@ -18,4 +17,15 @@ export function getFile(name: string): string {
     return folder + name;
 }
 
-export default folder;
+export interface Config {
+    filePath: string;
+    port: number;
+    token: string;
+}
+
+var config: Config = JSON.parse(fs.readFileSync(getFile("config.json"), "utf-8"));
+
+export function getConfig() {
+    return config;
+}
+
